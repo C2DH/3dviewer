5424b218b39b9b31b262ebcc36e99d3ef1c3de4d
Daniele Guido <gui.daniele@gmail.com>
2025-02-18 12:01:24 +0100

    Update Viewer.tsx

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -3,7 +3,7 @@ import GLBPreload from './components/GLBPreload'
 
 const Viewer = () => {
   const [searchParams] = useSearchParams()
-  const url = searchParams.get('url') || 'http://localhost:5174/demo.glb'
+  const url = searchParams.get('url') || '/demo.glb'
   if (!url) return <div>URL not found</div>
   return (
     <div className='h-screen w-screen'>

e8e34325f9563fbb39f7c31b441643ff04517a43
Daniele Guido <gui.daniele@gmail.com>
2025-02-18 11:16:53 +0100

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <h1>3dviewer</h1>
+    </div>
+  )
+}
+
+export default App
